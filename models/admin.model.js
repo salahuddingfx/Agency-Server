06ac42d5e6b021f366cb0d9256bff0404d9b1bdc
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-08 00:52:03 +0600

    feat: implement MongoDB schemas for User, Admin, Contact, and Application models

--- models/admin.model.js
+++ models/admin.model.js
@@ -15,7 +15,7 @@ const adminSchema = new mongoose.Schema(
       trim: true,
       lowercase: true,
       match: [
-        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/,
         'Please provide a valid email address',
       ],
     },
