eed09132ac69225b2ec35e3815e5a307c42b4b67
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-08 01:55:17 +0600

    feat: implement invoice controller with PDF generation and email notification logic

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -2,6 +2,7 @@ import Invoice from '../models/invoice.model.js';
 import Client from '../models/client.model.js';
 import * as factory from '../helpers/factory.helper.js';
 import { sendInvoiceEmail } from '../services/mail.service.js';
+import { generateInvoicePdf } from '../services/invoice.service.js';
 
 export const getInvoices = factory.getAll(Invoice, 'client');
 export const getInvoice = factory.getOne(Invoice, 'client');
@@ -10,10 +11,20 @@ export const createInvoice = async (req, res, next) => {
   try {
     const invoice = await Invoice.create(req.body);
     
-    // Find client to send email notification
+    // Find client to generate PDF and send email notification
     const client = await Client.findById(invoice.client);
-    if (client && client.email) {
-      await sendInvoiceEmail(client.email, invoice);
+    if (client) {
+      try {
+        const pdfUrl = await generateInvoicePdf(invoice, client);
+        invoice.pdfUrl = pdfUrl;
+        await invoice.save();
+      } catch (pdfError) {
+        console.error('PDF Generation failed during invoice creation:', pdfError.message);
+      }
+
+      if (client.email) {
+        await sendInvoiceEmail(client.email, invoice);
+      }
     }
 
     res.status(201).json({
