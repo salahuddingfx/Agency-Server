ddf77ce4e5e60e1819fcb767c6f85adcf2a6f301
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-08 00:52:25 +0600

    feat: implement CORS configuration and define contact Mongoose schema

--- config/cors.config.js
+++ config/cors.config.js
@@ -4,12 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.ADMIN_URL,
   'http://localhost:5173', // Client portal / main web app dev port
   'http://localhost:5174', // Admin portal dev port
   'https://admin.nextora.tech',
   'https://nextora.tech',
   'https://api.nextora.tech',
-];
+].filter(Boolean);
 
 const corsOptions = {
   origin: (origin, callback) => {
